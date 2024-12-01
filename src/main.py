b636406b76455cc1e7c25de913729d42ccca85e4
tiruka <tiruka.engineer@gmail.com>
2024-12-01 13:53:41 +0900

    add one hot model onnx by tensorflow

--- src/main.py
+++ src/main.py
@@ -1,36 +1,46 @@
 import tensorflow as tf
-import tf2onnx
 import subprocess
 
-# 簡単なTensorFlowモデルを定義
-class SimpleModel(tf.keras.Model):
-    def __init__(self):
-        super(SimpleModel, self).__init__()
-        self.dense = tf.keras.layers.Dense(10, activation='relu')
-    
-    def call(self, inputs):
-        return self.dense(inputs)
+# One-Hotモデル定義
+class OneHotModel(tf.keras.Model):
+    def __init__(self, depth, on_value=1.0, off_value=0.0, axis=-1):
+        super(OneHotModel, self).__init__()
+        self.depth = depth
+        self.on_value = on_value
+        self.off_value = off_value
+        self.axis = axis
+    @tf.function(input_signature=[tf.TensorSpec(shape=None, dtype=tf.int32)])
+    def __call__(self, indices):
+        return tf.one_hot(
+            # tf.constant([0, 2, -1, 1], dtype=tf.int32),
+            indices=indices,
+            depth=self.depth,
+            on_value=self.on_value,
+            off_value=self.off_value,
+            axis=self.axis
+        )
+# モデルの初期化
+depth = 3  # One-Hotエンコーディングの深さ
+model = OneHotModel(depth=depth, on_value=1.0, off_value=0.0, axis=-1)
 
-# モデルインスタンスを作成
-model = SimpleModel()
+# ダミー入力
+dummy_input = tf.constant([0, 2, 3, 1], dtype=tf.int32)  # 入力を整数型に変更
+print(dummy_input)
+output = model(dummy_input)  # 出力を確認
+print("TensorFlowモデル出力:\n", output)
 
-# モデルの保存ディレクトリ
-saved_model_dir = "simple_model"
-
-# モデルを保存する
-input_shape = (1, 5)  # 入力の形状 (バッチサイズ, 入力特徴数)
-dummy_input = tf.random.normal(input_shape)
-model(dummy_input)  # モデルを呼び出して初期化
+# モデルの保存
+saved_model_dir = "one_hot_saved_model"
+model_name = "one_hot.onnx"
 tf.saved_model.save(model, saved_model_dir)
-
+print("モデルを保存しました。")
 # ONNXへの変換
-
 # TensorFlowモデルをONNXに変換するためのコマンド
 command = [
     "python", "-m", "tf2onnx.convert",
-    "--saved-model", "simple_model",  # 保存したモデルのパス
-    "--output", "simple_model.onnx",  # 出力するONNXファイルのパス
-    "--opset", "13"  # 使用するONNXのバージョン
+    "--saved-model", saved_model_dir,  # 保存したモデルのパス
+    "--output", model_name,  # 出力するONNXファイルのパス
+    "--opset", "16",  # 使用するONNXのバージョン
 ]
 
 # コマンドを実行
